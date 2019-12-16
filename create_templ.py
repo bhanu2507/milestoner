08462b11086ada70434fc2915f686b9fb09c0c65
Bhanu Prakash Reddy Mokkala <bhanu.mokkala@ATMECS.CORP>
2019-12-16 21:19:12 +0530

    read file list and file

--- create_templ.py
+++ create_templ.py
@@ -2,13 +2,13 @@ import tornado.web
 import json
 import os
 
-def create_template(tjson):
-    with open(os.path.join("templates","templatetext.txt"), 'w') as outfile:
+def create_template(tjson, tname):
+    with open(os.path.join("templates", tname + ".txt"), 'w') as outfile:
         json.dump(tjson, outfile)
 
 class post_project_template(tornado.web.RequestHandler):
     def post(self):
         """ print(self.request.body) """
         print(json.loads(self.request.body))
-        create_template(json.loads(self.request.body))
+        create_template(json.loads(self.request.body), self.get_argument("tname") )
         self.write('successfully posted')
--- get_templ.py
+++ get_templ.py
@@ -1,11 +1,21 @@
 import tornado.web
-
+import json
+import os
+from os import listdir
+from os.path import isfile, join
 
 class get_template(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.set_header("Content-Type", "text/plain")
+        # fd = os.open("templates/"+self.get_argument("tname")+".txt",os.O_RDWR)
+        # readBytes = os.read(fd, 50)
+        # os.close(fd)
+        with open("templates/"+self.get_argument("tname")+".txt", "rb") as f:
+            byte = f.read()   
+        self.write(json.loads(byte))
 
 class get_template_list(tornado.web.RequestHandler):
     def get(self):
+        onlyfiles = [f for f in listdir('templates') if isfile(join('templates', f))]
         self.set_header("Content-Type", "text/plain")
-        self.write("You wrote " + self.get_body_argument("message"))
+        self.write(json.dumps(onlyfiles))
