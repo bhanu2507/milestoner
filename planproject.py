f857569b363ffbd65a7ca24995fa5c5f95dea3b0
Bhanu Prakash Reddy Mokkala <bhanu.mokkala@ATMECS.CORP>
2019-12-27 14:56:48 +0530

    basic plan

--- planproject.py
+++ planproject.py
@@ -5,17 +5,20 @@ from datetime import timedelta
 
 class plan_project(tornado.web.RequestHandler):
     def post(self):
+        planDict = {'final_plan': {'duration': 'none', 'completion_date': 'none', 'plan': 'none'}}
         self.set_header("Content-Type", "text/plain")
         try:
             with open("templates/"+self.get_argument("tname")+".txt", "rb") as f:
-                # pplan = json.load(f.read())
-                # print(pplan['project_template'][0])
                 jloop = json.loads(f.read())
                 duration = 0  
             for plan in jloop['project_template']:
                 duration = duration + plan['duration']
-            print(duration)
-            print(datetime.strptime(self.get_argument('sdate'), "%m/%d/%Y") + timedelta(duration))
-            self.write(jloop['project_template'][0])    
+                plan['task_completion_date'] = 'test'
+            # print(duration)
+            # print(datetime.strptime(self.get_argument('sdate'), "%m/%d/%Y") + timedelta(duration))
+            planDict['final_plan']['duration'] = duration
+            planDict['final_plan']['completion_date'] = json.dumps(datetime.strptime(self.get_argument('sdate'), "%m/%d/%Y") + timedelta(duration), default=str)
+            planDict['final_plan']['plan'] = jloop['project_template']
+            self.write(planDict) 
         except:
             self.write("Cannot open the file or no file exists")
